954b22ce5b0476e0cac0786e1e38bdc5378b5250
Thejas S <thejassivakumar@gmail.com>
2025-09-20 11:06:30 +0530

    hero component

--- src/Components/hero/hero.jsx
+++ src/Components/hero/hero.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
 import './hero.css'
 import { Link } from 'react-scroll'
-const hero = () => {
 
-  const handleScroll = () => {
-    const section = document.getElementById("programs"); // 👈 target section
-    section?.scrollIntoView({ behavior: "smooth" });
-  };
-
+const Hero = () => {
   return (
     <div className='hero container'>
-        <div className="hero-text">
-          <h1>Get scalable and secure solutions</h1>
-          <p>High quality products in less time and lesser cost</p>
-          <button className='btn'>
+      <div className="hero-left">
+        <h1 className="hero-name">Thejas S</h1>
+        <p className="hero-role">Software Engineer</p>
+      </div>
+
+      <div className="hero-right">
+        <h1>Get scalable and secure solutions</h1>
+        <p>High quality products in less time and lesser cost</p>
+        <button className='btn'>
           <Link to="programs" smooth={true} offset={-260} duration={600}>
             Explore more
           </Link>
-          </button>
-        </div>
+        </button>
+      </div>
     </div>
   )
 }
 
-export default hero
+export default Hero
